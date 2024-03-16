4021aac98c788c4cf7eafbbb9d42253fb1b0e6e2
Aditya S <aditya.srikanth1511@gmail.com>
2024-03-16 10:17:51 +0530

    requirement and setup file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="visa_approval_prediction",
+    version="0.0.0",
+    author="Aditya",
+    author_email="aditya.srikanth1511@gmail.com",
+    packages=find_packages()
+)
