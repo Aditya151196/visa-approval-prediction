bd6c80c825a89a6d0f74c449476e1f82eae0ddce
Aditya S <aditya.srikanth1511@gmail.com>
2024-03-23 18:53:40 +0530

    EDA notebook added

--- demo.py
+++ demo.py
@@ -1,3 +1,11 @@
 from us_visa.logger import logging
+from us_visa.exception import USVisaException
+import sys
 
-logging.info("Custom log working fine")
+try:
+    logging.info("Entering try block")
+    result = 1/"0"
+except Exception as e:
+    logging.error(USVisaException(e,sys))
+
+
